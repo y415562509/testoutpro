b9246d5f93b2b8e1d8260666e564f6d0d2c6f860
dawei <415562509@qq.com>
2020-04-08 13:28:42 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      name: ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
